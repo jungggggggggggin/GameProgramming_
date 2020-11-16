f2d3e582069a8e7628481b01c2c41d5827f6e736
jungggggggggggin <37722217+jungggggggggggin@users.noreply.github.com>
2020-11-16 22:10:29 +0900

    Add files via upload

---
+++ 2020.11.16/9장_행맨 - 원본/words1.js
@@ -0,0 +1,3 @@
+var words = [
+	 "sheep","qat"
+	  ];
---
+++ HW2/9장_행맨/words1.js
@@ -0,0 +1,5 @@
+var words = [
+	"dog", "cow","rabbit","goat", , "mouse", "horse", "deer", "monkey", 
+	"tiger", "wolf","fox","lion", "hyena","cheetah", "jaguar","elephant",
+	"pig", "bear", "panda", "seal", "whale", "dolphin"
+	  ];
